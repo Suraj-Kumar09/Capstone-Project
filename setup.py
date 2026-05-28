a6c47724d04d7d1b95a0b382f062649d7e3ef8f0
Suraj Kumar <srsuraj009@gmail.com>
2026-05-28 17:58:05 +0530

    project structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Capstone Project showcasing a production-style Flask monitoring and observability pipeline using Docker, Kubernetes (Minikube), Prometheus, and Grafana for real-time metrics collection and visualization.',
+    author='Suraj Kumar',
+    license='',
+)
